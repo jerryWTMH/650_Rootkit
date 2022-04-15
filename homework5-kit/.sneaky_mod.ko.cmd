ef7adcc1d13256e2274ad1038d9e391f6782c1d5
jerryWTMH <jerry1202.wtmh@gmail.com>
2022-04-15 10:55:06 -0400

    semifinal modify read

--- homework5-kit/.sneaky_mod.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ch450/650_Rootkit/homework5-kit/sneaky_mod.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000  --build-id  -T ./scripts/module-common.lds -o /home/ch450/650_Rootkit/homework5-kit/sneaky_mod.ko /home/ch450/650_Rootkit/homework5-kit/sneaky_mod.o /home/ch450/650_Rootkit/homework5-kit/sneaky_mod.mod.o;  true
